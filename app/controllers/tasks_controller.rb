b8c221ecb6738a6a62b71302e011d3f07c77fb04
wintan1418 <wintan1418@gmail.com>
2022-08-18 17:21:21 +0100

    Create private method and create action and notification for inputs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,23 @@ class TasksController < ApplicationController
         @tasks = Task.all
         @task = Task.new
     end
+
+    def create
+        @task = Task.new(task_params)
+
+        respond_to do |format|
+            if @task.save
+             format.html {redirect_to tasks_url, notice "You have successfully created a task"}
+            else
+                format.html {render :new, status :unprocessible_entity}
+
+    end
+ end
+end
+
+private
+
+def task_params
+    params.require(:task).permit(:description)
+ end
 end
