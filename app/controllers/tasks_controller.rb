d3657b57ff3c61691ac3476105953f430647e2b5
wintan1418 <wintan1418@gmail.com>
2022-08-19 13:44:01 +0100

    Fix error on template rendering and add checkbox to task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,16 +2,16 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all
         @task = Task.new
-    end
+      end
 
     def create
         @task = Task.new(task_params)
 
         respond_to do |format|
             if @task.save
-             format.html {redirect_to tasks_url, notice "You have successfully created a task"}
+             format.html {redirect_to tasks_url, notice: "You have successfully created a task"}
             else
-                format.html {render :new, status :unprocessible_entity}
+                format.html {render :new, status: "unprocessible_entity"}
 
     end
  end
