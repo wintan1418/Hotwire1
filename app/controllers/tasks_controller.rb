03c4ba8c417baf260f4de702c77bbdd5f8826b39
wintan1418 <wintan1418@gmail.com>
2022-08-19 14:02:25 +0100

    Complete crud action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,33 @@ class TasksController < ApplicationController
         @tasks = Task.all
         @task = Task.new
       end
-      
+
       def toggle
         @task = Task.find(params[:id])
         @task.update(completed: params[:completed])
       
         render json: { message: "Success" }
+      end
+
+      def edit
+        @task = Task.find(params[:id])
+      end
+    
+      def update
+        @task = Task.find(params[:id])
+        respond_to do |format|
+          if @task.update(task_params)
+            format.html { redirect_to tasks_url, notice: "Task was successfully updated" }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+      def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_url, notice: "Post was successfully deleted."
       end
 
     def create
